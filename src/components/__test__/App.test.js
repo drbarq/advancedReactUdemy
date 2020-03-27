ba3948849be6741ca05dd7d4987503519bc8d5a3
drbarq <J.Tustin@gmail.com>
2020-03-27 17:43:31 -0600

    test expectations

--- src/components/__test__/App.test.js
+++ src/components/__test__/App.test.js
@@ -1,3 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../App';
+
+it('shows a comment box', () => {
+	const div = document.createElement('div');
+
+	ReactDOM.render(<App />, div);
+	// looks inside div and check if the comment box is in there
+	expect(div.innerHTML).toContain('Comment Box');
+
+	ReactDOM.unmountComponentAtNode(div);
+});
